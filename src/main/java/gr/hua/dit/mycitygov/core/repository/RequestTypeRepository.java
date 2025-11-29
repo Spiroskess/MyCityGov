b9c694057924533e9c75cb0901141af47ce58fff
Spiroskess <spyroskesidis@gmail.com>
2025-11-29 16:00:56 +0200

    Initial commit

--- src/main/java/gr/hua/dit/mycitygov/core/repository/RequestTypeRepository.java
+++
@@ -1,7 +0,0 @@
-package gr.hua.dit.mycitygov.core.repository;
-
-import gr.hua.dit.mycitygov.core.model.RequestType;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RequestTypeRepository extends JpaRepository<RequestType, Long> {
-}
