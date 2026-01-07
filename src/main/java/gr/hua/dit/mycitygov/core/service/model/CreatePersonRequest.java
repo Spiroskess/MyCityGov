b6bffdb76a9b3a030df305643c00daf4188ca4ca
Spiroskess <spyroskesidis@gmail.com>
2026-01-07 21:50:02 +0200

    Ελεγχοι εγκυροτητας ΑΦΜ, ΑΜΚ ΚΑΙ ΚΩΔΙΚΟΥ

--- src/main/java/gr/hua/dit/mycitygov/core/service/model/CreatePersonRequest.java
+++ src/main/java/gr/hua/dit/mycitygov/core/service/model/CreatePersonRequest.java
@@ -4,6 +4,7 @@ import gr.hua.dit.mycitygov.core.model.PersonRole;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record CreatePersonRequest(
@@ -12,7 +13,25 @@ public record CreatePersonRequest(
     @NotNull @NotBlank @Size(max = 100) String firstName,
     @NotNull @NotBlank @Size(max = 100) String lastName,
     @NotNull @NotBlank @Size(max = 18) String mobilePhoneNumber,
-    @NotNull @NotBlank @Size(max = 11) String afm,
-    @NotNull @NotBlank @Size(max = 11) String amka,
-    @NotNull @NotBlank @Size(min = 4, max = 24) String rawPassword
+
+    @NotNull(message = "Το ΑΦΜ είναι υποχρεωτικό.")
+    @NotBlank(message = "Το ΑΦΜ είναι υποχρεωτικό.")
+    @Size(min = 9, max = 9, message = "Το ΑΦΜ πρέπει να έχει ακριβώς 9 ψηφία.")
+    @Pattern(regexp = "\\d+", message = "Το ΑΦΜ πρέπει να περιέχει μόνο ψηφία.")
+    String afm,
+
+    @NotNull(message = "Το ΑΜΚΑ είναι υποχρεωτικό.")
+    @NotBlank(message = "Το ΑΜΚΑ είναι υποχρεωτικό.")
+    @Size(min = 11, max = 11, message = "Το ΑΜΚΑ πρέπει να έχει ακριβώς 11 ψηφία.")
+    @Pattern(regexp = "\\d+", message = "Το ΑΜΚΑ πρέπει να περιέχει μόνο ψηφία.")
+    String amka,
+
+    @NotNull(message = "Ο κωδικός είναι υποχρεωτικός.")
+    @NotBlank(message = "Ο κωδικός είναι υποχρεωτικός.")
+    @Size(min = 9, max = 64, message = "Ο κωδικός πρέπει να έχει τουλάχιστον 9 χαρακτήρες.")
+    @Pattern(
+        regexp = "^(?=.*\\p{L})(?=.*[^\\p{L}\\d]).+$",
+        message = "Ο κωδικός πρέπει να περιέχει τουλάχιστον 1 γράμμα και 1 σύμβολο."
+    )
+    String rawPassword
 ) {}
